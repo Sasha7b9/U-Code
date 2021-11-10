b640c755d4996ac129d9925e5d1395d8340323d6
Sasha7b9 <Sasha7b9@tut.by>
2021-11-10 15:31:46 +0300

    Попытка ввести новый проект пока не увенчалась успехом

---
+++ scripts/win/install/edit.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set _BUILD_TYPE_=debug
+
+if "%1"=="" goto START
+if %1==release set _BUILD_TYPE_=release
+
+:START
+
+if %_BUILD_TYPE_%==release goto START_RELEASE
+
+:START_DEBUG
+@echo on
+start devenv.exe ../../../generated/Urho3D/%_BUILD_TYPE_%/Urho3D.sln
+goto EXIT
+
+:START_RELEASE
+@echo on
+start devenv.exe ../../../generated/Urho3D/%_BUILD_TYPE_%/Urho3D.sln /projectconfig Release
+
+:EXIT
