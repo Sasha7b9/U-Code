cec3d09b300413a25aafb5d7b39167923ed1e61e
Sasha7b9 <Sasha7b9@tut.by>
2021-11-11 21:50:19 +0300

    *

--- Tankist/scripts/install/ThirdParty/make_build_debug.bat
+++ Tankist/scripts/install/ThirdParty/make_build_debug.bat
@@ -1,4 +1,4 @@
-rmdir ..\..\..\generated\debug\ThirdParty /S /Q
+rmdir ../../../generated/debug/ThirdParty /S /Q
 
 cmake ../../../src/ThirdParty/CMakeLists.txt -B../../../generated/debug/ThirdParty -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_GENERATOR="Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Debug
 
--- Tankist/scripts/install/ThirdParty/make_build_release.bat
+++ Tankist/scripts/install/ThirdParty/make_build_release.bat
@@ -1,4 +1,4 @@
-rmdir ..\..\..\generated\release\ThirdParty /S /Q
+rmdir ../../../generated/release/ThirdParty /S /Q
 
 cmake ../../../src/ThirdParty/CMakeLists.txt -B../../../generated/release/ThirdParty -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_GENERATOR="Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release
 
