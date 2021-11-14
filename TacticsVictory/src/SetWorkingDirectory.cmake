e0aa7ac4082daf547b6d98213caa4082c0ef79f4
Sasha7b9 <Sasha7b9@tut.by>
2021-11-14 19:32:16 +0300

    *

--- TacticsVictory/src/SetWorkingDirectory.cmake
+++
@@ -1,5 +0,0 @@
-if(WIN32)
-    set_target_properties(
-        ${PROJECT_NAME} PROPERTIES
-        VS_DEBUGGER_WORKING_DIRECTORY ${_OUT_DIR_})
-endif()
--- TacticsVictory/src/TacticsVictoryCommon.cmake
+++ TacticsVictory/src/TacticsVictoryCommon.cmake
@@ -1,11 +1,29 @@
-macro(set_working_directory)
+macro(link_need_libraries)
+    if(USE_CPPFS)
+    
+        if(${CMAKE_BUILD_TYPE} STREQUAL Release)
+            target_link_libraries(${PROJECT_NAME} cppfs)
+        elseif(${CMAKE_BUILD_TYPE} STREQUAL Debug)
+            target_link_libraries(${PROJECT_NAME} cppfsd)
+        endif()
+        
+    endif()
+    if(UNIX)
+    
+        target_link_libraries(${PROJECT_NAME} pthread)
+        set_target_properties(${PROJECT_NAME} PROPERTIES
+            LINKER_LANGUAGE CXX
+        )
+    
+    endif()
+endmacro()
 
+macro(set_working_directory)
     if(MSVC)
         set_target_properties(
             ${PROJECT_NAME} PROPERTIES
             VS_DEBUGGER_WORKING_DIRECTORY ${_OUT_DIR_})
     endif()
-
 endmacro()
 
 
