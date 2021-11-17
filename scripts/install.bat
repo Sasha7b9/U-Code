42b4a4761a5c0a3f2c68cffe1f84f74732d40774
DESKTOP-KS43MLD\Sasha <Sasha7b9@tut.by>
2021-11-17 17:31:46 +0300

    *

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,6 +1,6 @@
 rem                                     Создаём символьные ссылки
-rem call proc/create_links.bat
-rem cd %_START_DIR_%
+call proc/create_links.bat
+cd %_START_DIR_%
 
 call assembly.bat full all
 cd %_START_DIR_%
